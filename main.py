a23e0ca79d5aefe69d24a0d954df6ff232b14e7a
Jake <56329025+jakeo-dev@users.noreply.github.com>
2023-09-21 11:14:51 -0700

    First

---
+++ main.py
@@ -0,0 +1,42 @@
+def convert_letter_to_num(letter):
+    switcher = {
+        "A": 0,
+        "B": 1,
+        "C": 2,
+        "D": 3,
+        "E": 4,
+        "F": 5,
+        "G": 6,
+        "H": 7,
+        "I": 8,
+        "J": 9,
+        "K": 10,
+        "L": 11,
+        "M": 12,
+        "N": 13,
+        "O": 14,
+        "P": 15,
+        "Q": 16,
+        "R": 17,
+        "S": 18,
+        "T": 19,
+        "U": 20,
+        "V": 21,
+        "W": 22,
+        "X": 23,
+        "Y": 24,
+        "Z": 25
+    }
+    return switcher.get(letter, 26)
+
+
+while True:
+    letters = [x for x in input("Enter the word: ").upper()].reverse()
+
+    num = 0
+    i = 0
+    for l in letters:
+        num = num + (convert_letter_to_num(l) * (26 ** i))
+        i += 1
+
+    print(num)
